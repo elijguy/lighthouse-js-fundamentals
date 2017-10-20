72fdf2cb5d035d01305893d567501dc9b68ffbfb
Elijah Guy <elijahguy17@yahoo.ca>
2017-10-20 00:22:59 +0000

    ingredients.js

---
+++ ingredients.js
@@ -0,0 +1,19 @@
+var ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+var i = 0;
+
+while (i<8)
+{
+  console.log(ingredients[i]);
+  i++;
+}
+
+for (var b = 0; b < 7; b++)
+{
+  console.log(ingredients[b]);
+}
+
+
+for (var c = 7; c>0; c--)
+{
+  console.log(ingredients[c]);
+}
